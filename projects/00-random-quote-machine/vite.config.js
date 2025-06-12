845b5e691fefbd51945023eb14274ea51e7a4539
GuillermoFraija <guillermo.fraija@gmail.com>
2025-06-12 11:41:44 -0400

    base url

--- projects/00-random-quote-machine/vite.config.js
+++ projects/00-random-quote-machine/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://GuillermoFraija.github.io/tree/main/projects/00-random-quote-machine",
+});
